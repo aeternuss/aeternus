a8aec22bffac49bc80374a1f3113d5d0a981ba06
wonxin <aeternus@aliyun.com>
2019-10-30 16:52:54 +0800

    oss force cp

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,7 @@ GIT_HEAD_ID_FILE=.travis_HEAD
 $OSSUTIL_BIN config -c oss_config -e $OSS_endpoint -i $OSS_accessKeyID -k $OSS_accessKeySecret
 
 # get prev HEAD ID 
-$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ oss://$OSS_bucket/$GIT_HEAD_ID_FILE .
+$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f oss://$OSS_bucket/$GIT_HEAD_ID_FILE .
 
 if [ -f $GIT_HEAD_ID_FILE ]; then
   read PREV_HEAD < $GIT_HEAD_ID_FILE
@@ -15,14 +15,14 @@ fi
 
 # update changed files
 while read -r file; do
-  $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ \
+  $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
     "$file" oss://$OSS_bucket/$(dirname "$file")
 done < <(git diff --name-only $PREV_HEAD HEAD)
 
 # update current head id
 git rev-parse HEAD > $GIT_HEAD_ID_FILE
 
-$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ \
+$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
   $GIT_HEAD_ID_FILE oss://$OSS_bucket/
 
 rm -rf oss_config oss_out/ ossutil64
