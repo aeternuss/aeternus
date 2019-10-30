81968007a230ba4ab381f5ef867ac570b11b5ff6
wonxin <aeternus@aliyun.com>
2019-10-30 16:30:23 +0800

    oss cp only changed files

---
+++ .travis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OSSUTIL_BIN=./ossutil64
+GIT_HEAD_ID_FILE=.travis_HEAD
+
+# get prev HEAD ID and current HEAD ID
+read PREV_HEAD < $GIT_HEADID_FILE
+
+# config ossutil
+$OSSUTIL_BIN config -c oss_config -e $OSS_endpoint -i $OSS_accessKeyID -k $OSS_accessKeySecret
+
+# update changed files
+while read -r file; do
+  $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ \
+    "$file" oss://$OSS_bucket/$(dirname "$file")
+done < <(git diff --name-only $PREV_HEAD HEAD)
+
+# update current head id
+git rev-parse HEAD > $GIT_HEAD_ID_FILE
+
+$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ \
+  $GIT_HEAD_ID_FILE oss://$OSS_bucket/
+
+rm -rf oss_config oss_out/ ossutil64
