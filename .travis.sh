d097c2c1adc8592d3a5b0565bdbc43e1c80d8f4c
aeternus <aeternus@aliyun.com>
2019-11-13 13:40:47 +0800

    remove  end / of oss://[secure]//

--- .travis.sh
+++ .travis.sh
@@ -12,25 +12,32 @@ $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
     oss://$OSS_bucket/$GIT_HEAD_ID_FILE ./
 
 if [ -f $GIT_HEAD_ID_FILE ]; then
-  read PREV_HEAD < $GIT_HEAD_ID_FILE
+    read PREV_HEAD < $GIT_HEAD_ID_FILE
 fi
 
-# update changed files
+# upload changed files
 while read -r file; do
-  echo "UPDATE: $file"
+    echo "UPLOAD: $file"
 
-  oss_dir=$(dirname "$file")
-  if [ "$oss_dir" == "." ]; then oss_dir=""; fi
+    # update oss_dir name
+    #     .          ->  NULL
+    #     ./sub/dir  ->  ./sub/dir/
+    oss_dir=$(dirname "$file")
+    if [ "$oss_dir" == "." ]; then
+        oss_dir=""
+    else
+        oss_dir="$oss_dir/"
+    fi
 
-  $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-    "$file" oss://$OSS_bucket/$oss_dir/
+    $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
+        "$file" oss://$OSS_bucket/$oss_dir
 done < <(git diff --name-only $PREV_HEAD HEAD)
 
 # update current head id
 git rev-parse HEAD > $GIT_HEAD_ID_FILE
 
 $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-  $GIT_HEAD_ID_FILE oss://$OSS_bucket/
+    $GIT_HEAD_ID_FILE oss://$OSS_bucket/
 
 # remove tmp files
 rm -rf oss_config oss_out/ ossutil64
