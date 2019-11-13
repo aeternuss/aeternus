6d6dd0df13f18dadc3c8eb3ad433eeced4b04f28
aeternus <aeternus@aliyun.com>
2019-11-13 15:31:55 +0800

    handle rename and deleted files

--- .travis.sh
+++ .travis.sh
@@ -6,12 +6,12 @@ GIT_HEAD_ID_FILE=.travis_HEAD
 # --------------------------------------
 # config ossutil
 $OSSUTIL_BIN config -c oss_config \
+    --output-dir oss_out/ \
     -e $OSS_endpoint -i $OSS_accessKeyID -k $OSS_accessKeySecret
 
 # --------------------------------------
 # get prev HEAD ID 
-$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-    oss://$OSS_bucket/$GIT_HEAD_ID_FILE ./
+$OSSUTIL_BIN cp -c oss_config -f oss://$OSS_bucket/$GIT_HEAD_ID_FILE ./
 
 if [ -f $GIT_HEAD_ID_FILE ]; then
     read PREV_HEAD < $GIT_HEAD_ID_FILE
@@ -19,29 +19,41 @@ fi
 
 # --------------------------------------
 # upload changed files
-while read -r file; do
-    echo "UPLOAD: $file"
+while read -r status name1 name2; do
+    case $status in
+        # files added or modified
+        A*|M*)
+            echo "UPLOAD: $name1"
+            $OSSUTIL_BIN cp -c oss_config -f "$name2" oss://$OSS_bucket/$name1
+            ;;
 
-    # get oss_dir
-    #     .          ->  NULL
-    #     ./sub/dir  ->  ./sub/dir/
-    oss_dir=$(dirname "$file")
-    if [ "$oss_dir" == "." ]; then
-        oss_dir=""
-    else
-        oss_dir="$oss_dir/"
-    fi
+        # files deleted
+        D*)
+            echo "DELETE: $name1"
+            $OSSUTIL_BIN rm -c oss_config -f oss://$OSS_bucket/$name1
+            ;;
 
-    $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-        "$file" oss://$OSS_bucket/$oss_dir
-done < <(git diff --name-only $PREV_HEAD HEAD)
+        # files renamed
+        R*)
+            echo "DELETE: $name1"
+            $OSSUTIL_BIN rm -c oss_config -f oss://$OSS_bucket/$name1
+
+            echo "UPLOAD: $name2"
+            $OSSUTIL_BIN cp -c oss_config -f "$name2" oss://$OSS_bucket/$name2
+            ;;
+
+        # others
+        *)
+            echo "UNKNOW: $name1:$name2"
+            exit -1
+            ;;
+    esac
+done < <(git diff --name-status $PREV_HEAD HEAD)
 
 # --------------------------------------
 # update current HEAD id
 git rev-parse HEAD > $GIT_HEAD_ID_FILE
-
-$OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-    $GIT_HEAD_ID_FILE oss://$OSS_bucket/
+$OSSUTIL_BIN cp -c oss_config -f $GIT_HEAD_ID_FILE oss://$OSS_bucket/
 
 # remove tmp files
 rm -rf oss_config oss_out/ ossutil64
