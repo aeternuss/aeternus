0ad3d653f198f189bfd500c62abf04ca01422093
aeternus <aeternus@aliyun.com>
2019-11-13 14:28:18 +0800

    correct oss_dir value

--- .travis.sh
+++ .travis.sh
@@ -3,10 +3,12 @@
 OSSUTIL_BIN=./ossutil64
 GIT_HEAD_ID_FILE=.travis_HEAD
 
+# --------------------------------------
 # config ossutil
 $OSSUTIL_BIN config -c oss_config \
     -e $OSS_endpoint -i $OSS_accessKeyID -k $OSS_accessKeySecret
 
+# --------------------------------------
 # get prev HEAD ID 
 $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
     oss://$OSS_bucket/$GIT_HEAD_ID_FILE ./
@@ -15,25 +17,27 @@ if [ -f $GIT_HEAD_ID_FILE ]; then
     read PREV_HEAD < $GIT_HEAD_ID_FILE
 fi
 
+# --------------------------------------
 # upload changed files
 while read -r file; do
     echo "UPLOAD: $file"
 
-    # update oss_dir name
+    # get oss_dir
     #     .          ->  NULL
     #     ./sub/dir  ->  ./sub/dir/
     oss_dir=$(dirname "$file")
-    #if [ "$oss_dir" == "." ]; then
-    #    oss_dir=""
-    #else
-    #    oss_dir="$oss_dir/"
-    #fi
+    if [ "$oss_dir" == "." ]; then
+        oss_dir=""
+    else
+        oss_dir="$oss_dir/"
+    fi
 
     $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
-        "$file" oss://$OSS_bucket/$oss_dir/
+        "$file" oss://$OSS_bucket/$oss_dir
 done < <(git diff --name-only $PREV_HEAD HEAD)
 
-# update current head id
+# --------------------------------------
+# update current HEAD id
 git rev-parse HEAD > $GIT_HEAD_ID_FILE
 
 $OSSUTIL_BIN cp -c oss_config --output-dir oss_out/ -f \
